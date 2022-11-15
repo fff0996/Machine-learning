c3219be6573db98282670538ce2b58913bd3b7bf
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-11-15 14:08:23 +0900

    Create Knn.py

---
+++ Knn.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+
+
+#Load Dataset
+dataset = pd.read_csv("dataset_path")
+
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Training and Predictions
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+# Evaluating the Algorithm
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+
+import itertools
+ranking = dict(itertools.islice(ranking.items(), n_features))
+top_snp = list(ranking.keys())
+top_snp = np.asarray(top_snp)
+fs = RandomForestClassifier(max_features = 20, n_estimators=20, random_state=41)
+fs.fit(X_train, y_train)
+ranking = dict(zip(snp, fs.feature_importances_))
+ranking = dict(itertools.islice(ranking.items(), n_features))
+top_snp = list(ranking.keys())
+top_snp = np.asarray(top_snp)
+
+X_train = data.drop(test_index)
+X_train = X_train[top_snp].values
+X_test = data.reindex(test_index)
+X_test = X_test[top_snp].values
+
+tmp = knn(X_train, y_train, X_test)
